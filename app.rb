6416b9d93f40068c3e09d2783c2036101f328d92
Keith Salisbury <keith.salisbury@alliants.com>
2015-06-11 11:24:37 +0100

    moved menu construction to ruby

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require 'opal-jquery'
 $current_target = nil
 
 Document.ready? do
+  menu = Element.find '#menu'
+  ['One','Two','Three'].each do |item_name|
+    menu.append "<li>#{item_name}</li>"
+  end
   Element.find('#menu li').on :click do |evt|
     $current_target.remove_class 'blue' unless $current_target.nil?
     evt.current_target.add_class 'blue'
