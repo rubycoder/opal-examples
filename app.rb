98ef78d2a28cc50bd0c34cc239675e1738c34c29
Keith Salisbury <keith.salisbury@alliants.com>
2015-06-15 20:22:29 +0100

    demonstrate http promises

--- app.rb
+++ app.rb
@@ -3,8 +3,11 @@ require 'opal-jquery'
 
 class MyWidget
 
+  @comments
+
   def initialize
-    HTTP.get('/some/json').then do |response|
+    @comments = []
+    HTTP.get('http://www.reddit.com/user/brain_poop/comments/.json').then do |response|
       process_json_data(response.json)
     end.fail do |response|
       handle_error(response.json[:error])
@@ -14,15 +17,34 @@ class MyWidget
   end
 
   def process_json_data(json)
+    alert('Data loaded successfully. Click ok to see the results!')
 
+    json['data']['children'].each do |child|
+      data = child['data']
+      div = Element.parse "
+      <div>
+        <h1>#{data['link_title']}</h1>
+        <p>#{data['body']}</p>
+      </div>
+      "
+      @comments << div
+    end
+
+    render_comments
   end
 
   def handle_error(json)
-
+    alert('handle_error')
   end
 
-  def hide_spinner
+  def hide_spinner!
+    Element['#spinner'].fade_toggle(800)
+  end
 
+  def render_comments
+    @comments.each do |comment|
+      Document.body << comment
+    end
   end
 end
 
